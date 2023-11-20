034331c32787febe070dc8e38ae1199d2adaecdf
david-borja <davidborjamartinez@gmail.com>
2023-11-20 06:34:24 +0100

    add: playlist detail navigation with transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  output: 'server', // tiene sentido que las páginas se rendericen en el servidor porque las playlists son infinitas, sería imposible generarlas todas de manera estática
+})
