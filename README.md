2e66e1bd8ec52149533db2c51fb3d23fbc019834
david-borja <davidborjamartinez@gmail.com>
2023-11-20 07:22:38 +0100

    add: greeting with svelte component

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Add tailwind
 bunx astro add tailwind
 ```
 
+Add svelte
+
+```sh
+bunx astro add svelte
+```
+
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/withastro/astro/tree/latest/examples/basics)
 [![Open with CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/sandbox/github/withastro/astro/tree/latest/examples/basics)
 [![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/withastro/astro?devcontainer_path=.devcontainer/basics/devcontainer.json)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server', // tiene sentido que las páginas se rendericen en el servidor porque las playlists son infinitas, sería imposible generarlas todas de manera estática
-})
+  integrations: [tailwind(), svelte()],
+  output: 'server' // tiene sentido que las páginas se rendericen en el servidor porque las playlists son infinitas, sería imposible generarlas todas de manera estática
+});
