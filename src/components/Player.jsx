17e3bd2b4ac779533ae80dfb1ec8f8754431674d
david-borja <davidborjamartinez@gmail.com>
2024-01-02 19:34:25 +0100

    add: current song to player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -27,6 +27,20 @@ export const Play = ({ className }) => (
   </svg>
 )
 
+const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div className={`flex items-center gap-5 relative overflow-hidden`}>
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title} />
+      </picture>
+      <div className="flex flex-col">
+        <h3 className="font-semibold text-sm block">{title}</h3>
+        <span className="text-xs opacity-80">{artists?.join(', ')}</span>
+      </div>
+    </div>
+  )
+}
+
 export function Player() {
   const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
     (state) => state
@@ -34,22 +48,27 @@ export function Player() {
   const audioRef = useRef()
 
   useEffect(() => {
-    audioRef.current.src = `/music/1/01.mp3`
-  }, [])
+    isPlaying ? audioRef.current.play() : audioRef.current.pause()
+  }, [isPlaying])
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`
+      audioRef.current.src = src
+      audioRef.current.play()
+    }
+  }, [currentMusic])
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause()
-    } else {
-      audioRef.current.play()
-      audioRef.current.volume = 0.5
-    }
     setIsPlaying(!isPlaying)
   }
 
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
-      <div>CurrentSong...</div>
+      <div>
+        <CurrentSong {...currentMusic.song} />
+      </div>
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
