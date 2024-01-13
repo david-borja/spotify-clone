5cd6c08842fec5acddaaf30e4898aaf1424046a4
david-borja <davidborjamartinez@gmail.com>
2024-01-13 14:40:15 +0100

    add: big play button to MusicsTable

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from './Player'
 import { usePlayerStore } from '@/store/playerStore'
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state)
 
@@ -31,12 +31,17 @@ export function CardPlayButton({ id }) {
     // setCurrentMusic({ songs, playlist, song: songs[0] })
   }
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
   return (
     <button
       onClick={handleClick}
-      className="card-play-button rounded-full bg-green-500 p-4"
+      className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   )
 }
